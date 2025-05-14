3c02317343a6328eedc02e98470651a554a977c4
Bernardo Mata <matabernardo2@gmail.com>
2025-05-14 11:46:10 +0530

    adding PromptTemplate to the research agent

--- API/backendRequest.py
+++ API/backendRequest.py
@@ -51,7 +51,35 @@ def get_response():
         raise HTTPException(status_code=404, detail="No hay textos procesados disponibles")
     
     client = genai.Client(api_key="AIzaSyBS0ERWhkYDIaMifZD1IWpFWGNtSyfZUPo")
+
+    #research prompt
+    prompt = f"""
+        Conduct a comprehensive search across the internet
+        for all available information regarding the following
+        supply chain risk management
+        problem provided by the user: {processed_texts[-1]}.
+        Ensure the output is in the same language as the user's input.
+        If the user's input is not related to supply chain risk management
+        (e.g., general greetings, unrelated definitions), 
+        respond with the message:
+
+        "My purpose is to provide solutions for risk management;
+        I am not qualified to provide this type of information." 
+
+        Include definitions, problem analysis, potential impact, 
+        contributing factors, and any other relevant information
+        discovered related to the specified supply chain risk.
+    """
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents=processed_texts[-1]  # Use the last processed text
+        model="gemini-2.0-flash-lite",
+        contents=prompt,
+        config={
+            "temperature": 0.1,
+            "max_output_tokens": 512,
+            "top_p": 0.8,
+            "top_k": 40,
+        }
     )
+
     return {"response": response.text}
