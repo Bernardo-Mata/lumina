69c8f0edadb1340dd9c93690f42c2766f4845bca
Bernardo Mata <matabernardo2@gmail.com>
2025-05-30 22:06:02 +0530

    fix: Correct duplicate FastAPI instance initialization in main.py

--- API/main.py
+++ API/main.py
@@ -5,7 +5,7 @@ from API.backendRequest import router as backend_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # O restringe a ["http://localhost:3000"] si usas React local
