1837daa6edb2b0f3f429028cbbcf5a04789ddb49
Bernardo Mata <matabernardo2@gmail.com>
2025-06-02 11:18:17 +0530

    hola

--- API/main.py
+++ API/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # O restringe a ["http://localhost:3000"] si usas React local
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
