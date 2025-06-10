6c1f26c8e661ff792cd804a1ba79969f1e917269
litzypesina <litzypesinar@gmail.com>
2025-06-10 04:54:40 -0600

    prueba del 10 de junio

--- API/database.py
+++ API/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:2016870@localhost/lumina"
+SQLALCHEMY_DATABASE_URL = "mysql://root:BL4CKB0R051LFL45k@@localhost/lumina"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)  
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
