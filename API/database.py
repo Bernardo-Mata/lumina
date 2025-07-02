e3a6d92b02a61cb29071a55eef0061dc501a516b
litzypesina <litzypesinar@gmail.com>
2025-07-02 03:39:54 -0600

    load environment variables for database connection

--- API/database.py
+++ API/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = ""
-engine = create_engine(SQLALCHEMY_DATABASE_URL)  
+load_dotenv(dotenv_path=".env.examples") 
+engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URL"), connect_args={"charset": "utf8mb4"})  
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
